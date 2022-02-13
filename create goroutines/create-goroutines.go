0362c19f55568037f83ee1cdd931c5d9cc3fbf29
Chris Siannas <xrsia@otenet.gr>
2022-02-13 11:49:36 +0200

    Outline

--- create goroutines/create-goroutines.go
+++ create goroutines/create-goroutines.go
@@ -1,5 +1,22 @@
 package main
 
 func main() {
+	/*
+		execute the function twice , sequentially
+		with arguments "chris" and "tasia"
+	*/
 
+	/*
+		execute the function:
+			first time as goroutine, argument "chris"
+			second time not as goroutine, argument "tasia"
+	*/
 }
+
+/*
+	create a function which:
+		get a string as parameter
+		iterates 3 times
+			in each iteration prints the iteration number,string
+		sleeps for 1 sec
+*/
